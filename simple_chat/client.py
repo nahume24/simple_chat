63682fd25ba868153f299959c6d3666aa399a2ff
giovanialtelino <galtelino@gmail.com>
2019-06-23 16:20:26 -0300

    conn working

--- simple_chat/client.py
+++ simple_chat/client.py
@@ -1,15 +1,29 @@
 from socket import socket, AF_INET, SOCK_STREAM
 import logging
-
+from threading import Thread
 
 class ChatClient:
-    def __init__(self):
+    def __init__(self, host, port):
         self.logger = self._setup_logger()
         self.sock = self._setup_socket(host, port)
 
+        thread = Thread(target=self.send_message)
+        thread.start()
+
+        while True:
+            data = self.sock.recv(4096)
+            if not data:
+                break
+            self.logger.info(data.decode())
+
+    def send_message(self):
+        while True:
+            user_message = input()
+            self.sock.send(user_message.encode('utf-8', 'blackslashreplace'))
+
     @staticmethod
     def _setup_logger():
-        logger = logging.getLogger('chat_server')
+        logger = logging.getLogger('chat_client')
         logger.addHandler(logging.StreamHandler())
         logger.setLevel(logging.DEBUG)
         return logger
@@ -17,6 +31,8 @@ class ChatClient:
     @staticmethod
     def _setup_socket(host, port):
         sock = socket(AF_INET, SOCK_STREAM)
-        sock.bind((host, port))
-        sock.listen()
+        sock.connect((host, port))
         return sock
+
+if __name__ == "__main__":
+    client = ChatClient('localhost', 4336)
--- simple_chat/server.py
+++ simple_chat/server.py
@@ -1,18 +1,37 @@
-from socket import socket, AF_INET, SOCK_STREAM
+from socket import socket, AF_INET, SOCK_STREAM, SOL_SOCKET, SO_REUSEADDR
 import logging
+from concurrent.futures import ThreadPoolExecutor
 
 
 class ChatServer:
     def __init__(self, host, port):
         self.logger = self._setup_logger()
         self.sock = self._setup_socket(host, port)
+        self.connections = []
 
     def run(self):
         self.logger.info("Chat server is running")
+        with ThreadPoolExecutor() as executor:
+            while True:
+                conn, addr = self.sock.accept()
+                self.logger.debug(f"new connection: {addr}")
 
+                self.connections.append(conn)
+                self.logger.debug(f"Connections: {self.connections}")
+
+                executor.submit(self.relay_messages, conn, addr)
+
+    def relay_messages(self, conn, addr):
         while True:
-            conn, addr = self.sock.accept()
-            self.logger.DEBUG("new connection: {addr}")
+            data = conn.recv(4096)
+
+            for connection in self.connections:
+                a = str(addr[0]).encode('utf-8')
+                connection.send(a + data)
+            
+            if not data:
+                self.logger.warning("No Data. Exiting.")
+                break
 
     @staticmethod
     def _setup_logger():
@@ -24,6 +43,12 @@ class ChatServer:
     @staticmethod
     def _setup_socket(host, port):
         sock = socket(AF_INET, SOCK_STREAM)
+        sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
         sock.bind((host, port))
         sock.listen()
         return sock
+
+
+if __name__ == "__main__":
+    server = ChatServer('localhost', 4336)
+    server.run()
